80763803430b5f48200ca0fb2118798570112a94
unknown <davidrivera3096@gmail.com>
2021-11-12 11:13:41 -0600

    landing-js

---
+++ efectos.js
@@ -0,0 +1,8 @@
+// When the user scrolls down 80px from the top of the document, resize the navbar's padding and the logo's font size
+window.onscroll = function() {scrollFunction()};
+
+function scrollFunction() {
+  if (document.body.scrollTop > 200 || document.documentElement.scrollTop > 200) {
+    document.getElementById("SK").className += " animate__animated animate__pulse";
+  }
+}
